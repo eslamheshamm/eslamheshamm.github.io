6980f74f92e9daa9a6315f957a62e9562351f739
eslamheshamm <esl2mhesham@gmail.com>
2021-06-16 21:58:47 +0200

    adding gatsby

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# eslamheshamm.github.io
+**my personal website.**
---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	siteMetadata: {
+		title: "eslam-hesham",
+	},
+	plugins: [
+		"gatsby-plugin-postcss",
+		`gatsby-plugin-dark-mode`,
+		"gatsby-plugin-gatsby-cloud",
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
+};
