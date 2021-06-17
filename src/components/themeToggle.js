6d21130f2c350e9f62711919d258d344cdd674ac
eslamheshamm <esl2mhesham@gmail.com>
2021-06-17 18:24:02 +0200

    dark mode

--- src/components/themeToggle.js
+++ src/components/themeToggle.js
@@ -9,7 +9,7 @@ export default function ThemeToggle() {
 						type="checkbox"
 						className="hidden"
 						onChange={(e) => toggleTheme(e.target.checked ? "dark" : "light")}
-						checked={theme === "light"}
+						checked={theme === "dark"}
 					/>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -18,19 +18,19 @@ export default function ThemeToggle() {
 						stroke="currentColor"
 						className="w-4 h-4 text-gray-800 dark:text-gray-200"
 					>
-						{theme === "light" ? (
+						{theme === "dark" ? (
 							<path
 								strokeLinecap="round"
 								strokeLinejoin="round"
 								strokeWidth={2}
-								d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+								d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
 							/>
 						) : (
 							<path
 								strokeLinecap="round"
 								strokeLinejoin="round"
 								strokeWidth={2}
-								d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+								d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
 							/>
 						)}
 					</svg>
