100f35f0dc0dffac275977080d0c7d41fc10f09a
eslamheshamm <esl2mhesham@gmail.com>
2021-09-08 13:05:28 +0200

    update seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -17,20 +17,27 @@ const SEO = ({ title, description, image, article }) => {
 	const seo = {
 		title: title || defaultTitle,
 		description: description || defaultDescription,
-		image: `${siteUrl}${image || defaultImage}`,
+		image: `${image || defaultImage}`,
 		url: `${siteUrl}${pathname}`,
 	};
+	console.clear();
+	console.log(site.siteMetadata);
+	console.log(seo);
+	console.log(seo.image);
 	return (
 		<Helmet title={seo.title} titleTemplate={titleTemplate}>
+			{/* general tags */}
 			<meta name="description" content={seo.description} />
 			<meta name="image" content={seo.image} />
-			{seo.url && <meta property="og:url" content={seo.url} />}
+			{/* og tags */}
 			{(article ? true : null) && <meta property="og:type" content="article" />}
+			{seo.url && <meta property="og:url" content={seo.url} />}
 			{seo.title && <meta property="og:title" content={seo.title} />}
 			{seo.description && (
 				<meta property="og:description" content={seo.description} />
 			)}
 			{seo.image && <meta property="og:image" content={seo.image} />}
+			{/* twitter tags */}
 			<meta name="twitter:card" content="summary_large_image" />
 			{twitterUsername && (
 				<meta name="twitter:creator" content={twitterUsername} />
@@ -56,6 +63,7 @@ SEO.defaultProps = {
 	image: null,
 	article: false,
 };
+
 const query = graphql`
 	query SEO {
 		site {
