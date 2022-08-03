3f07b5d55f4661a0fa6a1b66d26d8640a90b5329
eslamheshamm <esl2mhesham@gmail.com>
2022-08-03 12:10:57 +0200

    Updates

--- component---src-pages-about-js-efe4c1a2a85047a047fd.js
+++ component---src-pages-about-js-efe4c1a2a85047a047fd.js
@@ -1,2 +1,34 @@
-(self.webpackChunkeslam_hesham=self.webpackChunkeslam_hesham||[]).push([[682],{5518:function(e,t,a){"use strict";a.r(t);var l=a(7294),s=a(3751),n=a(7364);t.default=function(){return l.createElement(l.Fragment,null,l.createElement(s.Z,{title:"About Me"}),l.createElement(n.Z,null,l.createElement("section",{className:"w-11/12 mx-auto mt-44"},l.createElement("h2",{className:"text-2xl uppercase text-center font-bold"},"I'm that guy who you'd like to hear some of his stories.")))," ")}}}]);
+(self.webpackChunkeslam_hesham = self.webpackChunkeslam_hesham || []).push([
+	[682],
+	{
+		5518: function (e, t, a) {
+			"use strict";
+			a.r(t);
+			var l = a(7294),
+				s = a(3751),
+				n = a(7364);
+			t.default = function () {
+				return l.createElement(
+					l.Fragment,
+					null,
+					l.createElement(s.Z, { title: "About Me" }),
+					l.createElement(
+						n.Z,
+						null,
+						l.createElement(
+							"section",
+							{ className: "w-10/12 lg:w-8/12 mx-auto mt-44" },
+							l.createElement(
+								"h2",
+								{ className: "text-2xl uppercase text-center font-bold" },
+								"I'm that guy who you'd like to hear some of his stories."
+							)
+						)
+					),
+					" "
+				);
+			};
+		},
+	},
+]);
 //# sourceMappingURL=component---src-pages-about-js-efe4c1a2a85047a047fd.js.map
